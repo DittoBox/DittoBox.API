e20ef608336dba43ea21cd974ef53c7c7139fd5f
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2024-11-03 00:59:27 -0500

    feat: NUni test project added

---
+++ DittoBox.API-Tets/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DittoBox.API_Tets
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
