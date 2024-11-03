4cde54172884e615ec966b3320cdd832507143e6
Luis  Eduardo Lagos Aguilar <112901601+Mirroy25@users.noreply.github.com>
2024-11-03 02:34:18 -0500

    docs: add documentation for AlertInstanceController methods

--- DittoBox.API/ContainerManagement/Interface/Controllers/AlertController.cs
+++ DittoBox.API/ContainerManagement/Interface/Controllers/AlertController.cs
@@ -10,12 +10,24 @@ namespace DittoBox.API.ContainerManagement.Interface.Controllers
     [Route("api/v1/[controller]")]
     public class AlertInstanceController : ControllerBase
     {
+         /// <summary>
+        /// Acknowledges an alert instance by its ID.
+        /// </summary>
+        /// <param name="alertInstanceId">The unique identifier of the alert instance to acknowledge.</param>
+        /// <param name="command">The command containing the details necessary to acknowledge the alert.</param>
+        /// <returns>A task representing the asynchronous operation, containing the acknowledged alert resource.</returns>
         [HttpPost]
         [Route("{alertInstanceId}/ack")]
         public Task<ActionResult<AlertResource>> AcknowledgeAlert([FromRoute]int alertId, [FromBody]AcknowledgeAlertCommand command)
         {
             throw new NotImplementedException();
         }
+          /// <summary>
+        /// Dismisses an alert instance by its ID.
+        /// </summary>
+        /// <param name="alertInstanceId">The unique identifier of the alert instance to dismiss.</param>
+        /// <param name="command">The command containing the details necessary to dismiss the alert.</param>
+        /// <returns>A task representing the asynchronous operation, containing the dismissed alert resource.</returns>
 
         [HttpPost]
         [Route("{alertInstanceId}/dismiss")]
@@ -23,6 +35,11 @@ namespace DittoBox.API.ContainerManagement.Interface.Controllers
         {
             throw new NotImplementedException();
         }
+        /// <summary>
+        /// Retrieves a list of active alerts.
+        /// </summary>
+        /// <param name="query">The query object containing the parameters for fetching active alerts.</param>
+        /// <returns>A task representing the asynchronous operation, containing a collection of active alert resources.</returns>
 
         [HttpGet]
         public Task<ActionResult<IEnumerable<AlertResource>>> GetActiveAlerts([FromBody]GetActiveAlertsQuery query)
