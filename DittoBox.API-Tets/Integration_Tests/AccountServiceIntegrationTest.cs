6cf0ffb87c125d4d970345b1db7a2557c8a75e7b
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2024-11-03 01:33:34 -0500

    feat: Service Integration tests

---
+++ DittoBox.API-Tets/Integration_Tests/AccountServiceIntegrationTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Moq;
+using DittoBox.API.AccountSubscription.Application.Services;
+using DittoBox.API.AccountSubscription.Domain.Models.Aggregates;
+using DittoBox.API.AccountSubscription.Domain.Repositories;
+using System.Threading.Tasks;
+
+namespace DittoBox.Tests.Integration
+{
+    [TestFixture]
+    public class AccountServiceTests
+    {
+        private Mock<IAccountRepository> _accountRepositoryMock;
+        private AccountService _accountService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _accountRepositoryMock = new Mock<IAccountRepository>();
+            _accountService = new AccountService(_accountRepositoryMock.Object);
+        }
+
+        [Test]
+        public async Task CreateAccount_ShouldAddAccount()
+        {
+            // Arrange
+            var representativeId = 1;
+            var businessName = "Test Business";
+            var businessId = "12345";
+
+            // Act
+            var account = await _accountService.CreateAccount(representativeId, businessName, businessId);
+
+            // Assert
+            _accountRepositoryMock.Verify(repo => repo.Add(It.IsAny<Account>()), Times.Once);
+            Assert.AreEqual(representativeId, account.RepresentativeId);
+            Assert.AreEqual(businessName, account.BusinessName);
+            Assert.AreEqual(businessId, account.BusinessId);
+        }
+    }
+}
---
+++ DittoBox.API-Tets/Integration_Tests/SubscriptionServiceIntegrationTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Moq;
+using DittoBox.API.AccountSubscription.Application.Services;
+using DittoBox.API.AccountSubscription.Domain.Models.Entities;
+using DittoBox.API.AccountSubscription.Domain.Repositories;
+using System.Threading.Tasks;
+
+namespace DittoBox.Tests.Integration
+{
+    [TestFixture]
+    public class SubscriptionServiceTests
+    {
+        private Mock<ISubscriptionRepository> _subscriptionRepositoryMock;
+        private SubscriptionService _subscriptionService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _subscriptionRepositoryMock = new Mock<ISubscriptionRepository>();
+            _subscriptionService = new SubscriptionService(_subscriptionRepositoryMock.Object);
+        }
+
+        [Test]
+        public async Task CreateSubscription_ShouldAddSubscription()
+        {
+            // Arrange
+            var accountId = 1;
+
+            // Act
+            var subscription = await _subscriptionService.CreateSubscription(accountId);
+
+            // Assert
+            _subscriptionRepositoryMock.Verify(repo => repo.Add(It.IsAny<Subscription>()), Times.Once);
+            Assert.AreEqual(accountId, subscription.AccountId);
+            Assert.AreEqual(0, subscription.TierId);
+            Assert.AreEqual(1, subscription.SubscriptionStatusId);
+        }
+    }
+}
