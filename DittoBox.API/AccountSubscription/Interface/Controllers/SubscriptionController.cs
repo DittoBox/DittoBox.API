c360f71a4fdf5ebb768f8ed3d17ec12ad106375b
Samuel Rolando Chamorro Torres <102174595+ChamorroSamuel@users.noreply.github.com>
2024-11-14 11:40:33 -0500

    Add XML documentation to SubscriptionController

--- DittoBox.API/AccountSubscription/Interface/Controllers/SubscriptionController.cs
+++ DittoBox.API/AccountSubscription/Interface/Controllers/SubscriptionController.cs
@@ -5,10 +5,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DittoBox.API.AccountSubscription.Interface.Controllers
 {
+    /// <summary>
+    /// Controller for managing subscription-related operations.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class SubscriptionController : ControllerBase
     {
+        /// <summary>
+        /// Retrieves details of a specific subscription.
+        /// </summary>
+        /// <param name="query">The query containing the subscription ID.</param>
+        /// <returns>A <see cref="SubscriptionResponse"/> with the subscription details.</returns>
         [HttpGet]
         [Route("{subscriptionId}")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SubscriptionResponse))]
@@ -19,6 +27,11 @@ namespace DittoBox.API.AccountSubscription.Interface.Controllers
             throw new System.NotImplementedException();
         }
 
+        /// <summary>
+        /// Upgrades an existing subscription to a higher tier.
+        /// </summary>
+        /// <param name="command">The command containing upgrade details.</param>
+        /// <returns>A <see cref="SubscriptionResponse"/> representing the upgraded subscription.</returns>
         [HttpPost]
         [Route("upgrade")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SubscriptionResponse))]
@@ -30,6 +43,11 @@ namespace DittoBox.API.AccountSubscription.Interface.Controllers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Downgrades an existing subscription to a lower tier.
+        /// </summary>
+        /// <param name="command">The command containing downgrade details.</param>
+        /// <returns>An <see cref="IActionResult"/> indicating the result of the operation.</returns>
         [HttpPost]
         [Route("downgrade")]
         public IActionResult DowngradeSubscription([FromBody] DowngradeSubscriptionCommand command)
@@ -37,6 +55,11 @@ namespace DittoBox.API.AccountSubscription.Interface.Controllers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Cancels an existing subscription.
+        /// </summary>
+        /// <param name="command">The command containing cancellation details.</param>
+        /// <returns>An <see cref="IActionResult"/> indicating the result of the operation.</returns>
         [HttpPost]
         [Route("cancel")]
         public IActionResult CancelSubscription([FromBody] CancelSubscriptionCommand command)
@@ -44,6 +67,11 @@ namespace DittoBox.API.AccountSubscription.Interface.Controllers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Retrieves the payment status of a specific subscription.
+        /// </summary>
+        /// <param name="query">The query containing the subscription ID.</param>
+        /// <returns>A <see cref="PaymentStatusResponse"/> with the subscription's payment status.</returns>
         [HttpGet]
         [Route("{subscriptionId}/status")]
         public ActionResult<PaymentStatusResponse> GetSubscriptionStatus([FromRoute]GetSubscriptionStatusQuery query)
