66ce00e3c2b045b72d9c4385fe5351d4dad663e9
Samuel Rolando Chamorro Torres <102174595+ChamorroSamuel@users.noreply.github.com>
2024-11-14 11:26:44 -0500

    Add XML documentation to AlertInstanceController

--- DittoBox.API/ContainerManagement/Interface/Controllers/AlertController.cs
+++ DittoBox.API/ContainerManagement/Interface/Controllers/AlertController.cs
@@ -6,10 +6,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DittoBox.API.ContainerManagement.Interface.Controllers
 {
+    /// <summary>
+    /// Controller for managing alert instances.
+    /// </summary>
     [ApiController]
     [Route("api/v1/[controller]")]
     public class AlertInstanceController : ControllerBase
     {
+        /// <summary>
+        /// Acknowledges a specific alert instance.
+        /// </summary>
+        /// <param name="alertId">The ID of the alert instance to acknowledge.</param>
+        /// <param name="command">The command containing acknowledgment details.</param>
+        /// <returns>An <see cref="AlertResource"/> representing the acknowledged alert.</returns>
         [HttpPost]
         [Route("{alertInstanceId}/ack")]
         public Task<ActionResult<AlertResource>> AcknowledgeAlert([FromRoute]int alertId, [FromBody]AcknowledgeAlertCommand command)
@@ -17,6 +26,12 @@ namespace DittoBox.API.ContainerManagement.Interface.Controllers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Dismisses a specific alert instance.
+        /// </summary>
+        /// <param name="alertId">The ID of the alert instance to dismiss.</param>
+        /// <param name="command">The command containing dismissal details.</param>
+        /// <returns>An <see cref="AlertResource"/> representing the dismissed alert.</returns>
         [HttpPost]
         [Route("{alertInstanceId}/dismiss")]
         public Task<ActionResult<AlertResource>> DismissAlert([FromRoute]int alertId, [FromBody]DismissAlertCommand command)
@@ -24,6 +39,11 @@ namespace DittoBox.API.ContainerManagement.Interface.Controllers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Retrieves all active alert instances based on the provided query.
+        /// </summary>
+        /// <param name="query">The query parameters for retrieving active alerts.</param>
+        /// <returns>A collection of active <see cref="AlertResource"/> instances.</returns>
         [HttpGet]
         public Task<ActionResult<IEnumerable<AlertResource>>> GetActiveAlerts([FromBody]GetActiveAlertsQuery query)
         {
