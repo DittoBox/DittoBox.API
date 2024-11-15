7cf109815b0f4acdea6ed94be53af1940fc84906
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2024-11-15 09:29:05 -0500

    feat: add Id and UserId properties to ProfileResource and update mapping logic

--- DittoBox.API/UserProfile/Application/Resources/ProfileResource.cs
+++ DittoBox.API/UserProfile/Application/Resources/ProfileResource.cs
@@ -4,10 +4,12 @@ namespace DittoBox.API.UserProfile.Application.Resources
 {
 	public record ProfileResource
 	{
+		public int Id { get; set; }
 		public string FirstName { get; set; } = string.Empty;
 		public string LastName { get; set; } = string.Empty;
 		public int? AccountId { get; set; }
 		public int? GroupId { get; set; }
+		public int UserId { get; set; }
 		public string[] Privileges { get; init; } = [];
 
 
@@ -15,10 +17,12 @@ namespace DittoBox.API.UserProfile.Application.Resources
 		{
 			return new ProfileResource()
 			{
+				Id = profile.Id,
 				FirstName = profile.FirstName,
 				LastName = profile.LastName,
 				AccountId = profile.AccountId,
 				GroupId = profile.GroupId,
+				UserId = profile.UserId,
 				Privileges = profile.ProfilePrivileges.Select(p => p.Privilege.ToString()).ToArray()};
 		}
 	}
