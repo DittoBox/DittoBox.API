a34d0e22a6f9437a5505e03feff3bff77b37edef
dcancho <d.cancho.c@gmail.com>
2024-11-17 01:00:13 -0500

    fix: use int for notificationresource

--- DittoBox.API/ContainerManagement/Interface/Controllers/NotificationController.cs
+++ DittoBox.API/ContainerManagement/Interface/Controllers/NotificationController.cs
@@ -6,7 +6,7 @@ using DittoBox.API.ContainerManagement.Interface.Resources;
 using DittoBox.API.Shared.Domain.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DittoBox.API.UserProfile.Interface
+namespace DittoBox.API.ContainerManagement.Interface.Controllers
 {
     [ApiController]
     [Route("api/v1/[controller]")]
@@ -22,9 +22,9 @@ namespace DittoBox.API.UserProfile.Interface
             {
                 AlertType = (AlertType)command.AlertType,
                 IssuedAt = DateTime.UtcNow,
-                AccountId = command.AccountId,
-                GroupId = command.GroupId,
-                ContainerId = command.ContainerId
+                AccountId = command.AccountId ?? 0,
+                GroupId = command.GroupId ?? 0,
+                ContainerId = command.ContainerId ?? 0
             };
             await notificationRepository.Add(notification);
             await unitOfWork.CompleteAsync();
@@ -33,7 +33,7 @@ namespace DittoBox.API.UserProfile.Interface
 
         [HttpGet]
         [Route("account/{accountId:int}")]
-        public async Task<ActionResult<ICollection<NotificationResource>>> GetAllNotificationsByAccount([FromRoute]int accountId)
+        public async Task<ActionResult<ICollection<NotificationResource>>> GetAllNotificationsByAccount([FromRoute] int accountId)
         {
             var notifications = await notificationRepository.GetAllNotificationsByAccount(accountId);
             return Ok(notifications.Select(NotificationResource.FromNotification));
